653774e65d3f650a72d63f19e481d17f1849ac89
sophie <sofibibilashvili524@gmail.com>
2025-02-08 14:05:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const user1 = "Giorgi";
+const age1 = 20;
+
+const user2 = "mariami"
+const age2 = 18
+
+const color1 = "black"
+const color2 = "blue"
+const color3 = "purple"
+
+
+const num1 = 9
+const num2 = -5
+const num3 = 15 * 3
+const num4 = 15 + 3
+const num5 = 15 - 3
+const num6 = 15 / 3
+
+const num7 = 123 * 654
+
+console.log(num7);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
